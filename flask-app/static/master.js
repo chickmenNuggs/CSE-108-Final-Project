d44eddc5956a9d5385aba619d7a86fe962ab5dd5
chickmenNuggs <user39930@gmail.com>
2026-04-26 00:12:23 -0700

    Added a bunch of stuff
    finally got stuffs to work in js and added other stuff

--- flask-app/static/master.js
+++ flask-app/static/master.js
@@ -1,5 +1,35 @@
 
+var brushSize;
+var brushType;
 
+
+function toggleHiddenLobby(page1, page2, page3) {
+
+    b1 = document.getElementById(page1);
+    b2 = document.getElementById(page2);
+    b3 = document.getElementById(page3);
+    p1 = document.getElementsByClassName(page1)[0];
+    p2 = document.getElementsByClassName(page2)[0];
+    p3 = document.getElementsByClassName(page3)[0];
+    
+    b1.classList.add('nav-active');
+    b2.classList.remove('nav-active');
+    b3.classList.remove('nav-active');
+
+    p1.classList.toggle('hidden');
+    p2.classList.add('hidden');
+    p3.classList.add('hidden');
+}
+
+function syncBrushSize(el1, el2){
+    el_1 = document.getElementById(el1);
+    el_2 = document.getElementById(el2);
+
+    el_2.value = el_1.value;
+}
+function getBrushSize(){
+    brushSize = document.getElementById('brush-range').value
+}
 
 /* Canvas Js Start*/
 
