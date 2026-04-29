8feac8d1b69a56a7f7dffb64f74f3f2f5d73dfdb
chickmenNuggs <user39930@gmail.com>
2026-04-28 23:13:44 -0700

    I finally got canvas working
    Ill try to add more functionality, but i got bare minimum done

--- flask-app/static/master.js
+++ flask-app/static/master.js
@@ -1,11 +1,160 @@
-var brushSize;
-var brushType;
 const in_to_px = 600;
 const cm_to_px = 236;
 const in_to_cm = 2.54;
-
 let prevUnitW;
 let prevUnitH;
+let brushSize = 10;
+let brushType = 'brush';
+let brushColor;
+let isDrawing = false;
+
+
+
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext('2d')
+create = document.getElementsByClassName('create-new')[0];
+w = document.getElementById('width');
+h = document.getElementById('height');         
+width = document.getElementById('w-measure');
+height = document.getElementById('h-measure');
+var x, y;
+// syncBrushSize();
+
+/* Canvas Js Start*/
+function canvasInit(){
+   if(width.value == 'px'){
+      console.log(w.value)
+      console.log(canvas.width)
+      canvas.width = w.value;
+   }
+   else if(width.value== 'in'){
+      w.value = (w.value*600);
+      let t = Math.round(w.value)
+      w.value = t
+      canvas.width = w.value;  
+   }
+   else if(width.value== 'cm'){
+      w.value = (w.value*236);
+      let t = Math.round(w.value)
+      w.value = t
+      canvas.width = w.value
+   }
+
+   if( height.value == 'px'){
+      canvas.height = h.value;
+   }
+   else if(height.value== 'in'){
+      h.value = (h.value*600);
+      let t = Math.round(h.value)
+      h.value = t
+      canvas.height = h.value;
+   }
+   else if(height.value== 'cm'){
+      h.value = (h.value*236);
+      let t = Math.round(h.value)
+      h.value = t
+      canvas.height = h.value
+   }
+
+   create.classList.add('hidden');
+   canvas.classList.remove('hidden');
+   
+   window.onbeforeunload = function() {
+   return "Data will be lost if you leave the page, are you sure?";
+};
+}
+function backWarn(){ window.onbeforeunload = function() { return "Data will be lost if you leave the page, are you sure?"; }; }
+
+function startDraw(){
+   isDrawing = true;
+   console.log(isDrawing)
+}
+function endDraw(){
+   isDrawing = false;
+   console.log(isDrawing)
+}
+
+canvas.addEventListener('mousemove', (e) =>{
+   const rect = canvas.getBoundingClientRect();
+
+   const scaleX = canvas.width / rect.width;
+   const scaleY = canvas.height / rect.height;
+
+   x = (e.clientX - rect.left) * scaleX;
+   y = (e.clientY - rect.top) * scaleY;
+
+   x = Math.round(x);
+   y = Math.round(y);
+   
+   if(isDrawing){
+      draw(x,y);
+   }
+})
+
+
+function draw (x, y) {
+   canvas = document.getElementById('myCanvas');
+   let ctx = canvas.getContext('2d');
+   syncColor();
+   if(!isDrawing){ return; }
+   console.log(x,y)
+   drawPencil(x,y,brushSize,brushColor)
+   
+};
+
+function drawPencil(x,y,rad, color){
+   ctx.beginPath();
+   ctx.arc(x,y,rad,0,Math.PI * 2);
+   ctx.fillStyle = color;
+   ctx.fill();
+   ctx.stroke();
+   ctx.closePath();
+}
+
+/* Color Selecter Js*/
+
+/* Color Selecter Js End*/
+
+function syncColor(){
+   brushColor = document.getElementById('selected-color').value;
+}
+
+function syncBrushSize(el1, el2){
+   el_1 = document.getElementById(el1);
+   el_2 = document.getElementById(el2);
+   
+   el_2.value = el_1.value;
+   brushSize = el_2.value;
+}
+
+function setBrush(){
+   brushType = 'brush';
+}
+
+function setErase(){
+   brushType = 'eraser'
+}
+
+function setPan(){
+   brushType = 'pan';
+}
+
+function setTri(){
+   brushType = 'tri';
+}
+
+function setSquare(){
+   brushType = 'sqr';
+}
+
+function setCircle(){
+   brushType = 'circ';
+}
+
+
+
+
+/* Canvas Js End*/
 
 function swapWidthHeight(){
    const el1 = document.getElementById('width');
@@ -141,72 +290,3 @@ function unitConversion(el){
    }
    getprevUnit(el)
 }
-
-
-/* Canvas Js Start*/
-    function canvasInit(){
-         canvas = document.getElementById('myCanvas');
-         create = document.getElementsByClassName('create-new')[0];
-         w = document.getElementById('width');
-         h = document.getElementById('height');         
-         width = document.getElementById('w-measure');
-         height = document.getElementById('h-measure');
-
-         if(width.value == 'px'){
-            canvas.width = w.value;
-         }
-         else if(width.value== 'in'){
-            w.value = (w.value*600);
-            let t = Math.round(w.value)
-            w.value = t
-            canvas.width = w.value;
-         }
-         else if(width.value== 'cm'){
-            w.value = (w.value*236);
-            let t = Math.round(w.value)
-            w.value = t
-            canvas.width = w.value
-         }
-
-         if( height.value == 'px'){
-            canvas.height = h.value;
-         }
-         else if(height.value== 'in'){
-            h.value = (h.value*600);
-            let t = Math.round(h.value)
-            h.value = t
-            canvas.height = h.value;
-         }
-         else if(height.value== 'cm'){
-            h.value = (h.value*236);
-            let t = Math.round(h.value)
-            h.value = t
-            canvas.height = h.value
-         }
-
-         create.classList.add('hidden');
-         canvas.classList.remove('hidden');
-
-         window.onbeforeunload = function() {
-         return "Data will be lost if you leave the page, are you sure?";
-};
-
-    }
-
-
-/* Color Selecter Js*/
-
-/* Color Selecter Js End*/
-
-
-function syncBrushSize(el1, el2){
-    el_1 = document.getElementById(el1);
-    el_2 = document.getElementById(el2);
-
-    el_2.value = el_1.value;
-}
-function getBrushSize(){
-    brushSize = document.getElementById('brush-range').value
-}
-
-/* Canvas Js End*/
