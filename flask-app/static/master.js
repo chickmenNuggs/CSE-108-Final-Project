1468c537c980d06309494a011f1f5768ee1ff799
chickmenNuggs <user39930@gmail.com>
2026-05-07 01:03:34 -0700

    Pushing Updated Canvas
    Updated logic
    -Updated how brush works
    -Updated dropdown menus
    -Started Export IMG
    
    To do (Canvas):
    -Finish Export
    -Make Saving to a db
    -Add in a color selector
    -Pan tool

--- flask-app/static/master.js
+++ flask-app/static/master.js
@@ -1,3 +1,5 @@
+/* Global Vars. DO NOT TOUCH*/
+
 const in_to_px = 600;
 const cm_to_px = 236;
 const in_to_cm = 2.54;
@@ -7,11 +9,9 @@ let brushSize = 10;
 let brushType;
 let brushColor;
 let isDrawing = false;
-let isSaved = false;
+let isSaved = true;
 let isShowing = false;
 var x, y;
-
-
 canvas = document.getElementById( 'myCanvas' );
 ctx = canvas.getContext( '2d' )
 create = document.getElementsByClassName( 'create-new' )[ 0 ];
@@ -22,68 +22,66 @@ height = document.getElementById( 'h-measure' );
 b = document.getElementById( 'brush' );
 e = document.getElementById( 'eraser' );
 p = document.getElementById( 'e-pan' );
+exp = document.getElementById('expMenu');
+let lastX, lastY;
 
-
-document.addEventListener( 'click', ( event ) =>
-{
-   if ( isShowing == false )
+/*Drop Down Menu Start*/ 
+   function showMenu ()
    {
-      hideMenu();
+      isShowing = true;
+      menu = document.getElementById( 'files' );
+      files.classList.remove( 'hidden' )
    }
-   isShowing = false;
-} );
+
+   function hideMenu ()
+   {
+      menu = document.getElementById( 'files' );
+      files.classList.add( 'hidden' )
+   }
+
+   document.addEventListener( 'click', ( event ) =>
+   {
+      menu = document.getElementById( 'files' );
+      clicker = document.getElementById('revelator')
+      if ( menu.matches(':hover') || clicker.matches(':hover')) {return;}
+      hideMenu();
+   } );
+/*Drop Down Menu End*/ 
 
 
-function showMenu ()
-{
-   isShowing = true;
-   menu = document.getElementById( 'files' );
-   files.classList.remove( 'hidden' )
-}
 
-function hideMenu ()
-{
-   menu = document.getElementById( 'files' );
-   files.classList.add( 'hidden' )
-}
+/* Save / Export Code Start*/
 
+exp.addEventListener('click', ( event )=> {
+   emenu = document.getElementsByClassName('exportBin')[0];
+   if(emenu.matches(":hover")){
+      return;
+   }
+   hideExp()
+})
 
-function clearCanvas ()
-{
-   ctx.clearRect( 0, 0, canvas.width, canvas.height )
+function showExp(){
+   exp.classList.remove('hidden');
    hideMenu();
 }
-
-function newCanvas ()
-{
-   if ( isSaved == false )
-   {
-      backWarn();
-   }
-   clearCanvas()
-   canvas.classList.add( 'hidden' )
-   create.classList.remove( 'hidden' )
+function hideExp(){
+   exp.classList.add('hidden');
 }
 
 function saveCanvas ()
 {
 
+   isSaved = true;
 }
+
 
 function exportCanvas ()
 {
-
+   isSaved = true;
+   hideExp()
 }
 
-function zoomIn ()
-{
-
-}
-function zoomOut ()
-{
-
-}
-
+/* Save / Export Code Start*/
 
 /* Canvas Js Start*/
 function canvasInit ()
@@ -147,13 +145,6 @@ function canvasInit ()
    }
 }
 
-function backWarn () { window.onbeforeunload = function () { return "Data will be lost if you leave the page, are you sure?"; }; }
-
-window.onbeforeunload = function ()
-{
-   return "Data will be lost if you leave the page, are you sure?";
-};
-
 function SetClientCanvas ( width, height )
 {
    canvas.width = width;
@@ -163,86 +154,152 @@ function SetClientCanvas ( width, height )
    canvas.classList.remove( 'hidden' );
 }
 
-function backWarn () { window.onbeforeunload = function () { return "Data will be lost if you leave the page, are you sure?"; }; }
 
-function startDraw ()
+function clearCanvas ()
 {
-   isSaved = false;
-   isDrawing = true;
-   console.log( brushColor )
-   console.log( brushType )
-}
-function endDraw ()
-{
-   isDrawing = false;
+   ctx.clearRect( 0, 0, canvas.width, canvas.height )
+   ctx.beginPath();
+   hideMenu();
 }
 
-canvas.addEventListener( 'mousemove', ( e ) =>
+function newCanvas ()
 {
-   const rect = canvas.getBoundingClientRect();
-
-   const scaleX = canvas.width / rect.width;
-   const scaleY = canvas.height / rect.height;
-
-   x = ( e.clientX - rect.left ) * scaleX;
-   y = ( e.clientY - rect.top ) * scaleY;
-
-
-   x = Math.round( x );
-   y = Math.round( y );
-
-   if ( isDrawing )
+   if ( isSaved == false )
    {
-      draw( x, y );
+      backWarn();
    }
-} )
+   clearCanvas()
+   canvas.classList.add( 'hidden' )
+   create.classList.remove( 'hidden' )
+}
+
+
+// Begin Drawing
+canvas.addEventListener('mousedown', (e) => {
+   isDrawing = true;
+   [lastX, lastY] = [e.offsetX, e.offsetY];
+
+})
+// Moving Brush
+canvas.addEventListener( 'mousemove', ( e ) =>
+   {
+      //Making sure person is trying to draw
+      if(!isDrawing){
+         return;
+      }
+      // syncBrushSize();
+      // syncColor();
+      //getting bounds for canvas
+      const rect = canvas.getBoundingClientRect();
+      const scaleX = canvas.width / rect.width;
+      const scaleY = canvas.height / rect.height;
+      
+   
+      x = ( e.clientX - rect.left ) * scaleX;
+      y = ( e.clientY - rect.top ) * scaleY;
+      
+      
+      x = Math.round( x );
+      y = Math.round( y );
+      
+      //actually starting drawing
+      if ( isDrawing )
+         {
+            ctx.beginPath();
+            // draw( x, y, e );
+            drawkill(e, x, y)
+         }
+      } )
+   
+      // Showing Brush
+      function drawkill(e,x,y ){
+         if(!isDrawing) return;
+
+         ctx.beginPath();
+         ctx.moveTo(lastX, lastY);
+         ctx.lineTo(x, y);
+
+         if(brushType == "brush"){
+            ctx.strokeStyle = brushColor;
+         }
+         else if(brushType == 'e'){
+            ctx.strokeStyle = '#ffffff';
+         }
+         ctx.lineWidth = brushSize;
+         ctx.lineCap = 'round';
+         ctx.lineJoin = 'round';
+
+         ctx.stroke();
+
+         [lastX, lastY] = [x, y];
+      }
+      
+      function draw ( x, y , e)
+      {
+         syncColor(); 
+         ctx.lineJoin = 'round';
+         ctx.lineCap = 'round';
+         // console.log(x,y);
+      
+         if ( brushType == "brush" )
+         {
+            drawPencil( x, y, brushColor, e);
+            AddClientData( x, y, brushSize, brushColor );
+         }
+         else if ( brushType == 'e' )
+         {
+            drawPencil( x, y,"#ffffff", e );
+            AddClientData( x, y, brushSize, "#ffffff" );
+         }
+      };
+      
+      function drawPencil ( x, y, color, e)
+      {  
+
+         ctx.lineWidth = brushSize;
+         ctx.lineCap = 'round';
+         ctx.strokeStyle = brushColor;
+
+         ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+         ctx.stroke();
+         ctx.beginPath();
+         ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+      
+         // ctx.arc( x, y, rad, 0, Math.PI * 2 );
+         // ctx.fillStyle = color;
+         // ctx.fill();
+         // ctx.closePath();
+      }
+      // End Brush
+      
+      canvas.addEventListener('mouseup', (e) =>{
+         ctx.beginPath();
+         ForceSyncBoard();
+         isDrawing = false;
+      })
+      
+      canvas.addEventListener('mouseout', (e)=> {
+         ctx.beginPath();
+         isDrawing = false;
+      })
+      
+      
+      // For Syncing? Idk some websocket stuff.
 
 canvas.addEventListener( "mouseup", ( e ) =>
 {
    ForceSyncBoard();
 } );
 
-function draw ( x, y )
-{
-   canvas = document.getElementById( 'myCanvas' );
-   let ctx = canvas.getContext( '2d' );
-   syncColor();
-   // console.log(x,y);
 
-   if ( brushType == "brush" )
-   {
-      drawPencil( x, y, brushSize, brushColor );
-      AddClientData( x, y, brushSize, brushColor );
-   }
-   else if ( brushType == 'e' )
-   {
-      drawPencil( x, y, brushSize, "#ffffff" );
-      AddClientData( x, y, brushSize, "#ffffff" );
-   }
-};
 
-function drawPencil ( x, y, rad, color )
-{
-   ctx.beginPath();
-   ctx.arc( x, y, rad, 0, Math.PI * 2 );
-   ctx.fillStyle = color;
-   ctx.fill();
-   ctx.closePath();
-   /* 
-      ctx.moveTo(lastX, lastY);
-      ctx.lineTo(e.offsetX, e.offsetY);
-       ctx.strokeStyle = '#000'; // Brush color
-       ctx.lineWidth = 5;         // Brush size
-       ctx.lineCap = 'round';     // Makes ends smooth
-       ctx.stroke();
-       [lastX, lastY] = [e.offsetX, e.offsetY]; 
-   */
-}
 
 
 /* Color Selecter Js*/
 
 /* Color Selecter Js End*/
+
+/* Misc Helper functions*/ 
 
 function syncColor ()
 {
@@ -257,7 +314,6 @@ function syncBrushSize ( el1, el2 )
    el_2.value = el_1.value;
    brushSize = el_2.value;
 }
-
 window.addEventListener( 'load', ( event ) =>
 {
    console.log( "Setting brush type" );
@@ -265,15 +321,6 @@ window.addEventListener( 'load', ( event ) =>
    //syncBrushSize()
    syncColor()
 } );
-
-window.onload = function ()
-{
-   console.log( "Setting brush type" );
-   brushType = 'brush';
-   //syncBrushSize()
-   syncColor()
-}
-
 function setBrush ()
 {
    if ( brushType != 'brush' )
@@ -281,9 +328,6 @@ function setBrush ()
       b.classList.add( 'active-brush' );
       e.classList.remove( 'active-brush' );
       p.classList.remove( 'active-brush' );
-      // t.classList.remove('active-brush');
-      // c.classList.remove('active-brush');
-      // s.classList.remove('active-brush');  
    }
    brushType = 'brush';
 }
@@ -295,9 +339,6 @@ function setErase ()
       b.classList.remove( 'active-brush' );
       e.classList.add( 'active-brush' );
       p.classList.remove( 'active-brush' );
-      // t.classList.remove('active-brush');
-      // c.classList.remove('active-brush');
-      // s.classList.remove('active-brush');  
    }
    brushType = 'e';
 }
@@ -306,7 +347,6 @@ function setPan ()
 {
    brushType = 'pan';
 }
-
 function swapWidthHeight ()
 {
    const el1 = document.getElementById( 'width' );
@@ -323,7 +363,6 @@ function swapWidthHeight ()
    un2.value = un1.value;
    un1.value = t2;
 }
-
 function getprevUnit ( el )
 {
    var unit = document.getElementById( el );
@@ -343,7 +382,6 @@ function getprevUnit ( el )
 
    }
 }
-
 function unitConversion ( el )
 {
    unit = document.getElementById( el );
@@ -476,6 +514,17 @@ function unitConversion ( el )
 }
 
 
+function zoomIn ()
+{
+
+}
+function zoomOut ()
+{
+
+}
+
+
+/* Misc Helper functions end */ 
 
 
 // Put Roster info in here //
